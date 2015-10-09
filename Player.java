ab29e04fb7f49ba3c2540efd5855c207c1059285
Robert Ying <rbtying@aeturnalus.com>
2015-10-09 17:13:01 -0400

    Efficient collision detection

--- Player.java
+++ Player.java
@@ -1,155 +1,62 @@
 package pb.g3;
 
-import org.omg.CORBA.DynAnyPackage.Invalid;
 import pb.sim.Asteroid;
-import pb.sim.InvalidOrbitException;
 import pb.sim.Orbit;
 import pb.sim.Point;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Player implements pb.sim.Player {
 
-    private static class Key {
-        private final double mass;
-        private final long epoch;
-        private final double orbit_a;
-        private final double orbit_b;
-        private final double orbit_A;
-        private final double orbit_Mo;
-        private int hash;
+    private static double min_distance = Double.MAX_VALUE;
+    private static Map<Util.Key, Long> cache_perihelion = new ConcurrentHashMap<>();
+    private static Map<Util.Key, Long> cache_aphelion = new ConcurrentHashMap<>();
+    // used to pick asteroid and velocity boost randomly
+	private Random random = new Random();
+	// current time, time limit
+	private long time = -1;
+	private long time_limit = -1;
+    private long time_skip = -1;
+    private int num_asteroids = -1;
+    private PriorityQueue<Util.Push> next_pushes = new PriorityQueue<>();
 
-        private static Map<Asteroid, Key> cache = new ConcurrentHashMap<>();
-
-        public static Key factory(Asteroid a) {
-            return cache.computeIfAbsent(a, Key::new);
-        }
-
-        private Key(Asteroid a) {
-            mass = a.mass;
-            epoch = a.epoch;
-            orbit_a = a.orbit.a;
-            orbit_b = a.orbit.b;
-            orbit_A = a.orbit.A;
-            orbit_Mo = a.orbit.Mo;
-            hash = computeHashCode();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof Key)) {
-                return false;
-            }
-
-            Key ok = (Key) o;
-            return mass == ok.mass && epoch == ok.epoch && orbit_a == ok.orbit_a && orbit_b == ok.orbit_b && orbit_A == ok.orbit_A && orbit_Mo == ok.orbit_Mo;
-        }
-
-        private int computeHashCode() {
-            int hash = 17;
-            hash = hash * 23 + Double.hashCode(mass);
-            hash = hash * 23 + Double.hashCode(epoch);
-            hash = hash * 23 + Double.hashCode(orbit_a);
-            hash = hash * 23 + Double.hashCode(orbit_b);
-            hash = hash * 23 + Double.hashCode(orbit_A);
-            hash = hash * 23 + Double.hashCode(orbit_Mo);
-            return hash;
-        }
-
-        @Override
-        public int hashCode() {
-            return hash;
-        }
-    }
-
-    private class Push implements Comparable<Push>{
-        public int asteroid_idx;
-        public long push_time;
-        public double energy;
-        public double direction;
-        public double mass;
-        private Asteroid _simulated;
-
-        public Push(Asteroid a, int aidx, long pt, double e) {
-            double dir = a.orbit.velocityAt(pt - a.epoch).direction();
-            if (e < 0) {
-                dir += Math.PI;
-            }
-            init(aidx, pt, Math.abs(e), dir, a.mass);
-        }
-
-        public Push(int aidx, long pt, double e, double d, double m) {
-            init(aidx, pt, e, d, m);
-        }
-
-        private void init(int aidx, long pt, double e, double d, double m) {
-            asteroid_idx = aidx;
-            push_time = pt;
-            energy = e;
-            direction = d;
-            mass = m;
-            _simulated = null;
-        }
-
-        public Asteroid simulatedAsteroid(Asteroid a[]) {
-            if (_simulated == null) {
-                _simulated = Asteroid.push(a[asteroid_idx], push_time, energy, direction);
-            }
-            return _simulated;
-        }
-
-        public String toString() {
-            return String.format("Push: Asteroid %d at time %d with energy %f in direction %f", asteroid_idx, push_time, energy, direction);
-        }
-
-        @Override
-        public int compareTo(Push o) {
-            return (int) (push_time - o.push_time);
-        }
-    }
-
+    /**
+     * Computes the energyAtTime needed to transfer asteroid a to radius b when
+     * launching from its apoapsis
+     *
+     * @param a   Asteroid to transfer
+     * @param r_b Radius to transfer to
+     * @return Signed energyAtTime requirement
+     */
     public static double hohmannTransfer(Asteroid a, double r_b) {
         // transfers Asteroid a to the aphelion distance of b
         // and hopes for the best
-        double r_a = a.orbit.positionAt(findPerihelion(a)).magnitude();
+        double r_a = a.orbit.positionAt(findApoapsis(a)).magnitude();
 
         double dv = Math.sqrt(Orbit.GM / r_a) * (Math.sqrt(2 * r_b / (r_a + r_b)) - 1);
         return Math.copySign(a.mass * dv * dv * 0.5, dv);
     }
 
+    /**
+     * Computes the energyAtTime needed to transfer asteroid a from radius b
+     *
+     * @param a
+     * @param r_b
+     * @return
+     */
     public static double reverseHohmannTransfer(Asteroid a, double r_b) {
-        double r_a = a.orbit.positionAt(findPerihelion(a)).magnitude();
+        double r_a = a.orbit.positionAt(findPeriapsis(a)).magnitude();
 
         double dv = Math.sqrt(Orbit.GM / r_b) * (1 - Math.sqrt(2 * r_a / (r_a + r_b)));
         return Math.copySign(a.mass * dv * dv * 0.5, dv);
     }
 
-	// used to pick asteroid and velocity boost randomly
-	private Random random = new Random();
-
-	// current time, time limit
-	private long time = -1;
-	private long time_limit = -1;
-    private long time_skip = -1;
-
-    private PriorityQueue<Push> next_pushes = new PriorityQueue<>();
-
-    private static double min_distance = Double.MAX_VALUE;
-
-	// print orbital information
-	public void init(Asteroid[] asteroids, long time_limit)
-	{
-		if (Orbit.dt() != 24 * 60 * 60)
-			throw new IllegalStateException("Time quantum is not a day");
-		this.time_limit = time_limit;
-	}
-
-    private static Map<Key, Long> cache_perihelion = new ConcurrentHashMap<>();
-    private static Map<Key, Long> cache_aphelion = new ConcurrentHashMap<>();
-
-    private static long findPerihelion(Asteroid a) {
-        Key k = Key.factory(a);
+    private static long findPeriapsis(Asteroid a) {
+        Util.Key k = Util.Key.factory(a);
         Point p = new Point();
         return (cache_perihelion.computeIfAbsent(k, (x) -> Util.findArgMin(0, a.orbit.period(), (y) -> {
             a.orbit.positionAt(y, p);
@@ -157,16 +64,25 @@ public class Player implements pb.sim.Player {
         })) + a.epoch) % a.orbit.period();
     }
 
-    private static long findAphelion(Asteroid a) {
+    private static long findApoapsis(Asteroid a) {
         // maximum distance point is half the orbit away from the minimum distance point
-        return (findPerihelion(a) + a.orbit.period() / 2) % a.orbit.period();
+        return (findPeriapsis(a) + a.orbit.period() / 2) % a.orbit.period();
     }
 
-    private Push moveToRadius(long push_time, int aidx, Asteroid a, double radius) {
+    // print orbital information
+    public void init(Asteroid[] asteroids, long time_limit) {
+        if (Orbit.dt() != 24 * 60 * 60) {
+            throw new IllegalStateException("Time quantum is not a day");
+        }
+        this.time_limit = time_limit;
+        this.num_asteroids = asteroids.length;
+    }
+
+    private Util.Push moveToRadius(long push_time, int aidx, Asteroid a, double radius) {
         double r_a = a.orbit.positionAt(push_time - a.epoch).magnitude();
         double normalization_energy = reverseHohmannTransfer(a, r_a); // make it a circle?
         double hohmann_energy = hohmannTransfer(a, radius); // instantaneously ellipsize it
-        return new Push(a, aidx, push_time, hohmann_energy + normalization_energy);
+        return new Util.Push(a, aidx, push_time, hohmann_energy + normalization_energy);
     }
 
 	// try to push asteroid
@@ -174,99 +90,104 @@ public class Player implements pb.sim.Player {
 	                 double[] energy, double[] direction) {
         ++time;
 
+        if (num_asteroids != asteroids.length) {
+            next_pushes.clear();
+            System.out.println(String.format("#asteroids changed from %d to %d, dropping queued pushes", num_asteroids,
+                    asteroids.length));
+            num_asteroids = asteroids.length;
+        }
+
         if (time_skip > time) {
             return;
         } else {
             time_skip = -1;
         }
+        if (!next_pushes.isEmpty()) {
+            Util.Push next_push = next_pushes.peek();
+            if (time < next_push.push_time) {
+                return;
+            }
+            if (time == next_push.push_time) {
+                // apply push
+                energy[next_push.asteroid_idx] = next_push.energy;
+                direction[next_push.asteroid_idx] = next_push.direction;
 
-        try {
-            if (!next_pushes.isEmpty()) {
-                Push next_push = next_pushes.peek();
-                if (time < next_push.push_time) {
+                System.out.println("Making push " + next_push);
+
+                next_pushes.remove();
+                return;
+            }
+        } else {
+            System.out.println("no next push, computing more");
+
+            Integer indexes[] = new Integer[asteroids.length];
+            Integer radius_indexes[] = new Integer[asteroids.length];
+            for (int i = 0; i < indexes.length; ++i) {
+                indexes[i] = i;
+                radius_indexes[i] = i;
+            }
+
+            Arrays.sort(radius_indexes, (o1, o2) -> {
+                double d = Util.positionAt(asteroids[o2], time).magnitude() -
+                        Util.positionAt(asteroids[o1], time).magnitude();
+                return (int) Math.signum(d);
+            });
+
+            Arrays.sort(indexes, (o1, o2) -> {
+                double d = asteroids[o2].mass - asteroids[o1].mass;
+                return (int) Math.signum(d);
+            });
+
+            // grab largest asteroid
+            Asteroid largest = asteroids[indexes[0]];
+
+            double r_largest_ph = largest.orbit.positionAt(findPeriapsis(largest)).magnitude();
+            double r_largest_ap = largest.orbit.positionAt(findApoapsis(largest)).magnitude();
+            double r_largest = (r_largest_ap + r_largest_ph) / 2;
+
+            Util.Push potential_pushes[] = new Util.Push[indexes.length];
+            for (int i = 0; i < potential_pushes.length; ++i) {
+                potential_pushes[i] = null;
+            }
+
+            for (int i = 1; i < indexes.length; ++i) {
+                Asteroid a = asteroids[indexes[i]];
+                long push_time = time + 1;
+                Util.Push next_push = moveToRadius(push_time, indexes[i], a, r_largest);
+
+                Asteroid a2 = next_push.simulatedAsteroid(asteroids);
+                long t = a2.orbit.period();
+                long collision_time = push_time + t / 2;
+
+                if (Util.findCollision(a2, largest, time, time + 10000) >= 0) {
+                    if (potential_pushes[i] == null || next_push.energy < potential_pushes[i].energy) {
+                        potential_pushes[i] = next_push;
+                    }
+                }
+
+                if (potential_pushes[i] != null) {
+                    next_pushes.add(potential_pushes[i]);
+                    System.out.println("computed a next push: " + next_pushes.peek());
                     return;
                 }
-                if (time == next_push.push_time) {
-                    // apply push
-                    energy[next_push.asteroid_idx] = next_push.energy;
-                    direction[next_push.asteroid_idx] = next_push.direction;
+                break;
+            }
 
-                    System.out.println("Making push " + next_push);
+            // no pushes computed
 
-                    next_pushes.remove();
-                    return;
-                }
-            } else {
-                System.out.println("no next push, computing more");
-
-                Integer indexes[] = new Integer[asteroids.length];
-                for (int i = 0; i < indexes.length; ++i) {
-                    indexes[i] = i;
-                }
-
-                Arrays.sort(indexes, (o1, o2) -> {
-                    double d = asteroids[o2].mass - asteroids[o1].mass;
-                    if (d > 0) {
-                        return 1;
-                    } else if (d < 0) {
-                        return -1;
-                    } else {
-                        return 0;
-                    }
-                });
-
-                // grab largest asteroid
-                Asteroid largest = asteroids[indexes[0]];
-
-                double r_largest_ph = largest.orbit.positionAt(findPerihelion(largest)).magnitude();
-                double r_largest_ap = largest.orbit.positionAt(findAphelion(largest)).magnitude();
-                double r_largest = (r_largest_ap + r_largest_ph) / 2;
-
-                Push potential_pushes[] = new Push[indexes.length];
-                for (int i = 0; i < potential_pushes.length; ++i) {
-                    potential_pushes[i] = null;
-                }
-
-                for (int i = 1; i < indexes.length; ++i) {
-                    Asteroid a = asteroids[indexes[i]];
-
-                    for (long push_time = time + 1; push_time < time + largest.orbit.period() * 2; ++push_time) {
-                        Push next_push = moveToRadius(push_time, indexes[i], a, r_largest);
-                        Asteroid a2 = next_push.simulatedAsteroid(asteroids);
-                        long t = a2.orbit.period();
-                        long collision_time = push_time + t / 2;
-
-                        Point potential_collision_point = a2.orbit.positionAt(collision_time - a2.epoch);
-                        Point largest_location = largest.orbit.positionAt(collision_time - largest.epoch);
-
-                        if (Point.distance(potential_collision_point, largest_location) < a.radius() + largest.radius()) {
-                            if (potential_pushes[i] == null || next_push.energy < potential_pushes[i].energy) {
-                                potential_pushes[i] = next_push;
-                            }
-                        }
-                    }
-
-                    if (potential_pushes[i] != null) {
-                        next_pushes.add(potential_pushes[i]);
-                        System.out.println("computed a next push: " + next_pushes.peek());
-                        return;
-                    }
-                }
-                for (int i = 0; i < asteroids.length; ++i) {
-                    double ecc = Math.sqrt(1 - Math.pow(asteroids[i].orbit.b / asteroids[i].orbit.a, 2));
-                    double r_ph = largest.orbit.positionAt(findPerihelion(largest)).magnitude();
-                    double r_ap = largest.orbit.positionAt(findAphelion(largest)).magnitude();
-                    double r = (r_ap + r_ph) / 2;
-                    if (ecc > 0.05) {
-                        Push reversePush = new Push(asteroids[i], i, time + 1, reverseHohmannTransfer(largest, r));
-                        next_pushes.add(reversePush);
-                    } else {
-                        time_skip = largest.orbit.period();
-                    }
+            for (int i = 0; i < asteroids.length; ++i) {
+                double ecc = Math.sqrt(1 - Math.pow(asteroids[i].orbit.b / asteroids[i].orbit.a, 2));
+//                double r_ph = largest.orbit.positionAt(findPeriapsis(largest)).magnitude();
+                double r_ap = asteroids[i].orbit.positionAt(findApoapsis(asteroids[i])).magnitude();
+//                double r = (r_ap + r_ph) / 2;
+                if (ecc > 0.01) {
+                    Util.Push reversePush = new Util.Push(asteroids[i], i, findApoapsis(asteroids[i]),
+                            reverseHohmannTransfer(asteroids[i], r_ap));
+                    next_pushes.add(reversePush);
+                } else {
+                    time_skip = largest.orbit.period();
                 }
             }
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            next_pushes.clear();
         }
     }
 }
--- Util.java
+++ Util.java
@@ -1,31 +1,99 @@
 package pb.g3;
 
 import pb.sim.Asteroid;
+import pb.sim.Point;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 /**
  * Created by rbtying on 10/6/15.
  */
 public class Util {
-    public static long findArgMin(long start, long end, Function<Long, Double> f) {
-        long idx = start;
-        double minval = Double.MAX_VALUE;
-        for (long l = start; l < end; ++l) {
-            double test = f.apply(l);
-            if (test < minval) {
-                minval = test;
-                idx = l;
+
+
+    /**
+     * Returns the time of the collision between asteroids between [initial_time, max_time)
+     * <p>
+     * runs in O(shorter_orbit_period + num_intersection_points)
+     *
+     * @param a            the first asteroid to collide
+     * @param b            the second asteroid to collide
+     * @param initial_time
+     * @param max_time
+     * @return -1 if collision not found, otherwise, time of collision
+     */
+    public static long findCollision(Asteroid a, Asteroid b, long initial_time, long max_time) {
+        Asteroid shorter_orbit = a;
+        Asteroid longer_orbit = b;
+        if (b.orbit.period() < a.orbit.period()) {
+            shorter_orbit = b;
+            longer_orbit = a;
+        }
+
+        // if more than 5 points intersect per real intersection, something is very wrong
+        ArrayList<Long> potential_intersection_points = new ArrayList<>(512);
+
+        Point p1 = new Point(), p2 = new Point(), focus = new Point();
+        longer_orbit.orbit.center(focus);
+        focus.x *= 2;
+        focus.y *= 2;
+
+        double collision_distance = shorter_orbit.radius() + longer_orbit.radius();
+
+        long T = shorter_orbit.orbit.period();
+
+        for (long time = 0; time < T && initial_time + time < max_time; ++time) {
+            long t = time + initial_time;
+            positionAt(shorter_orbit, t, p1);
+            double d1 = Point.distance(p1, focus);
+            double r = p1.magnitude();
+            if (Math.abs(d1 + r - 2 * longer_orbit.orbit.a) < collision_distance) {
+                potential_intersection_points.add(time);
             }
         }
-        return idx;
+
+        // potential_intersection_points now contains up to num_intersections points to check
+        // points in potential_intersection_points are epoch-adjusted
+        long base_offset = initial_time;
+
+        while (base_offset < max_time) {
+            for (Long intersection_point : potential_intersection_points) {
+                long t = base_offset + intersection_point;
+                positionAt(shorter_orbit, t, p1);
+                positionAt(longer_orbit, t, p2);
+                double dist = Point.distance(p1, p2);
+                if (dist < collision_distance) {
+                    System.out.println("Found a collision!");
+                    return t;
+                }
+            }
+            // check in the next period
+            base_offset += T;
+        }
+
+        return -1;
     }
 
-    public static double findMin(long start, long end, Function<Long, Double> f) {
-        long idx = findArgMin(start, end, f);
-        return f.apply(idx);
+    /**
+     * Computes the eccentricity of an asteroid
+     *
+     * @param a
+     * @return
+     */
+    public static double eccentricity(Asteroid a) {
+        return Math.sqrt(1 - Math.pow(a.orbit.b / a.orbit.a, 2));
     }
 
+    /**
+     * Computes the argmax of f from i=start to i=end
+     * @param start initial value of long
+     * @param end one more than last value of long
+     * @param f f: long -> double
+     * @return
+     */
     public static long findArgMax(long start, long end, Function<Long, Double> f) {
         long idx = start;
         double maxval = Double.MIN_VALUE;
@@ -39,14 +107,201 @@ public class Util {
         return idx;
     }
 
+    /**
+     * Computes the argmin of f from i=start to i=end
+     *
+     * @param start initial value of long
+     * @param end   one more than last value of long
+     * @param f     f: long -> double
+     * @return
+     */
+    public static long findArgMin(long start, long end, Function<Long, Double> f) {
+        long idx = start;
+        double minval = Double.MAX_VALUE;
+        for (long l = start; l < end; ++l) {
+            double test = f.apply(l);
+            if (test < minval) {
+                minval = test;
+                idx = l;
+            }
+        }
+        return idx;
+    }
+
+    /**
+     * Computes the min of f from i=start to i=end
+     *
+     * @param start initial value of long
+     * @param end   one more than last value of long
+     * @param f     f: long -> double
+     * @return
+     */
+    public static double findMin(long start, long end, Function<Long, Double> f) {
+        long idx = findArgMin(start, end, f);
+        return f.apply(idx);
+    }
+
+
+    /**
+     * Computes the max of f from i=start to i=end
+     * @param start initial value of long
+     * @param end one more than last value of long
+     * @param f f: long -> double
+     * @return
+     */
     public static double findMax(long start, long end, Function<Long, Double> f) {
         long idx = findArgMax(start, end, f);
         return f.apply(idx);
     }
 
-    public static double energy(Asteroid a, long t) {
-        double vel = a.orbit.velocityAt(t - a.epoch).magnitude();
-        return 0.5 * a.mass * vel * vel;
+    /**
+     * Computes the epoch-adjusted velocity of an asteroid at a known time
+     *
+     * @param a the asteroid
+     * @param t the time
+     * @return epoch-adjusted velocity
+     */
+    public static Point velocityAt(Asteroid a, long t) {
+        return a.orbit.velocityAt(t - a.epoch);
+    }
+
+    public static void velocityAt(Asteroid a, long t, Point p) {
+        a.orbit.velocityAt(t - a.epoch, p);
+    }
+
+    /**
+     * Computes the epoch-adjusted position of an asteroid at a known time
+     *
+     * @param a the asteroid
+     * @param t the time
+     * @return epoch-adjusted position
+     */
+    public static Point positionAt(Asteroid a, long t) {
+        return a.orbit.positionAt(t - a.epoch);
+    }
+
+    public static void positionAt(Asteroid a, long t, Point p) {
+        a.orbit.positionAt(t - a.epoch, p);
+    }
+
+    /**
+     * Computes the epoch-adjusted kinetic energy at a time
+     *
+     * @param a the asteroid
+     * @param t the time
+     * @return epoch-adjusted kinetic energy
+     */
+    public static double energyAtTime(Asteroid a, long t) {
+        return energyFromVelocity(a, velocityAt(a, t).magnitude());
+    }
+
+    /**
+     * Computes the amount of energy necessary to introduce delta V of v on a
+     *
+     * @param a the asteroid
+     * @param v the velocity
+     */
+    public static double energyFromVelocity(Asteroid a, double v) {
+        return 0.5 * a.mass * v * v;
+    }
+
+    public static class Key {
+        private static Map<Asteroid, Key> cache = new ConcurrentHashMap<>();
+        private final double mass;
+        private final long epoch;
+        private final double orbit_a;
+        private final double orbit_b;
+        private final double orbit_A;
+        private final double orbit_Mo;
+        private int hash;
+
+        private Key(Asteroid a) {
+            mass = a.mass;
+            epoch = a.epoch;
+            orbit_a = a.orbit.a;
+            orbit_b = a.orbit.b;
+            orbit_A = a.orbit.A;
+            orbit_Mo = a.orbit.Mo;
+            hash = computeHashCode();
+        }
+
+        public static Key factory(Asteroid a) {
+            return cache.computeIfAbsent(a, Key::new);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Key)) {
+                return false;
+            }
+
+            Key ok = (Key) o;
+            return mass == ok.mass && epoch == ok.epoch && orbit_a == ok.orbit_a && orbit_b == ok.orbit_b &&
+                    orbit_A == ok.orbit_A && orbit_Mo == ok.orbit_Mo;
+        }
+
+        private int computeHashCode() {
+            int hash = 17;
+            hash = hash * 23 + Double.hashCode(mass);
+            hash = hash * 23 + Double.hashCode(epoch);
+            hash = hash * 23 + Double.hashCode(orbit_a);
+            hash = hash * 23 + Double.hashCode(orbit_b);
+            hash = hash * 23 + Double.hashCode(orbit_A);
+            hash = hash * 23 + Double.hashCode(orbit_Mo);
+            return hash;
+        }
+
+        @Override
+        public int hashCode() {
+            return hash;
+        }
+    }
+
+    public static class Push implements Comparable<Push> {
+        public int asteroid_idx;
+        public long push_time;
+        public double energy;
+        public double direction;
+        public double mass;
+        private Asteroid _simulated;
+
+        public Push(Asteroid a, int aidx, long pt, double e) {
+            double dir = a.orbit.velocityAt(pt - a.epoch).direction();
+            if (e < 0) {
+                dir += Math.PI;
+            }
+            init(aidx, pt, Math.abs(e), dir, a.mass);
+        }
+
+        public Push(int aidx, long pt, double e, double d, double m) {
+            init(aidx, pt, e, d, m);
+        }
+
+        private void init(int aidx, long pt, double e, double d, double m) {
+            asteroid_idx = aidx;
+            push_time = pt;
+            energy = e;
+            direction = d;
+            mass = m;
+            _simulated = null;
+        }
+
+        public Asteroid simulatedAsteroid(Asteroid a[]) {
+            if (_simulated == null) {
+                _simulated = Asteroid.push(a[asteroid_idx], push_time, energy, direction);
+            }
+            return _simulated;
+        }
+
+        public String toString() {
+            return String.format("Push: Asteroid %d at time %d with energyAtTime %f in direction %f", asteroid_idx,
+                    push_time, energy, direction);
+        }
+
+        @Override
+        public int compareTo(Push o) {
+            return (int) (push_time - o.push_time);
+        }
     }
 
 }
